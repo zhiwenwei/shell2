082400b7b3683acdcbe15226f979b1f593758ea9
David.Zhi <ddzhiwenwei@163.com>
2020-12-03 09:58:37 +0800

    auto install shadowsock

---
+++ auto_install_shadowsock.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+Server_IP=$(hostname -I)
+yum install -y python-setuptools wget
+wget https://pypi.python.org/packages/source/p/pip/pip-1.3.1.tar.gz --no-check-certificate -O /mnt/pip-1.3.1.tar.gz
+cd /mnt && tar xf pip-1.3.1.tar.gz
+cd pip-1.3.1 && python setup.py install
+pip install shadowsocks
+echo -e "{  
+ \"server\":\"$Server_IP\",
+ \"local_address\":\"127.0.0.1\",  
+ \"local_port\":1080,  
+ \"port_password\": {  
+     \"9994\": \"WWW.163.com\"
+ },  
+ \"timeout\":600,  
+ \"method\":\"rc4-md5\",  
+ \"fast_open\": false 
+}" > /etc/shadowsocks.json
+ssserver -c /etc/shadowsocks.json -d start
+echo "ssserver -c /etc/shadowsocks.json -d start" >> /etc/rc.d/rc.local
